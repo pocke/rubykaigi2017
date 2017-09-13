6c177f2b0c2dcc75c9d9f607321fa6448d11801d
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2017-09-13 19:23:23 +0900

    Add lint.rb

---
+++ lint.rb
@@ -0,0 +1,28 @@
+require 'parser/current'
+
+def traverse(node, detector)
+  detector.__send__(:"on_#{node.type}", node)
+  node.children.each do |child|
+    traverse(child, detector) if child.is_a?(Parser::AST::Node)   
+  end
+end
+
+class Detector
+  def on_if(node)
+    cond = node.children.first
+    if cond.type == :str
+      warn "Do not use a String literal in condition!!! (#{cond.loc.line}:#{cond.loc.column})"
+    end
+  end
+
+  def method_missing(name, *args)
+    if name.to_s.start_with?('on_')
+      # nothing
+    else
+      super
+    end
+  end
+end
+
+ast = Parser::CurrentRuby.parse(ARGF.read)
+traverse(ast, Detector.new)
